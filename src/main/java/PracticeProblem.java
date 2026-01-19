5cf95c888a9f1a1274f489f16dc7bc4a268c56a6
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-19 03:21:42 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,49 @@ public class PracticeProblem {
 
 	}
 
+	public static int searchMazeMoves(String [][] arr) {
+		int curRow = arr.length - 1;
+		int curCol = 0;
+		int no OfMoves = 0;
+		return minMoviesHelper(arr, curRow, curCol, noOfMoves);
+	}
+	public static int minMovesHelper(String[][] arr, int curRow, int curCol, int noOfMoves) {
+		if (curRow < 0 || curRow >= arr.length || curCol < 0 || curCol >- arr[0].length) {
+			return -1;
+		}
+		if (arr[curRow][curCol].equals("F")) {
+			return noOfMoves;
+		}
+		String temp = arr[curRow][curCol];
+		arr[curRow][curCol] = "*";
+		int right = minMovesHelper(arr, curRow, curCol + 1, noOfMoves + 1);
+		int up = minMovesHelper(arr, curRow - 1, curCol, noOfMoves + 1);
+		int left = minMovesHelper(arr, curRow, curCol -1, noOfMoves + 1);
+		int down = minMovesHelper (arr, curRow + 1, curCol, noOfMoves + 1);
+		arr[curRow][curCol] = temp;
+		int min = -1;
+		if (right != -1) min = right;
+		if (up != -1 && (min == -1 || up < min)) min = up;
+		if (left != -1 && (min == -1 || left < min)) min = left;
+		if (down != -1 && (min == -1 || down < min)) min = down;
+		return min;
+	}
+	public static int noOfPaths(String[][]arr, int curRow, int curCol) {
+		if (curRow < 0 || curRiw >= arr.length || curCol < 0 || curCol >= arr[0].length) {
+			return 0;
+		}
+		if (arr[curRow][curCol].equals("F")) {
+			return 1;
+		}
+		String temp = arr[curRow][curCol];
+		arr[curRow][curCol] = "*";
+		int right = pathsHelper(arr, curRow, curCol + 1);
+		int up = pathsHelper(arr, curRow - 1, curCol);
+		int left = pathsHelper(arr, curRow, curCol -1);
+		int down = pathsHelper(arr, curRow + 1, curCol);
+		arr[curRow][curCol] = temp;
+		return right + up + left + down;
+	}
+
 	
 }
