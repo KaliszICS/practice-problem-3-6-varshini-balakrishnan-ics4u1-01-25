4ea950ff59406c44263024924c1ad82e618ed8cb
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-19 03:54:41 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,6 +10,7 @@ public class PracticeProblem {
 		int noOfMoves = 0;
 		return minMovesHelper(arr, curRow, curCol, noOfMoves);
 	}
+	
 	public static int minMovesHelper(String[][] arr, int curRow, int curCol, int noOfMoves) {
 		if (curRow < 0 || curRow >= arr.length || curCol < 0 || curCol >= arr[0].length) {
 			return -1;
@@ -22,18 +23,20 @@ public class PracticeProblem {
 		}
 		String temp = arr[curRow][curCol];
 		arr[curRow][curCol] = "V";
+		
+		// Only explore right and up
 		int right = minMovesHelper(arr, curRow, curCol + 1, noOfMoves + 1);
 		int up = minMovesHelper(arr, curRow - 1, curCol, noOfMoves + 1);
-		int left = minMovesHelper(arr, curRow, curCol -1, noOfMoves + 1);
-		int down = minMovesHelper (arr, curRow + 1, curCol, noOfMoves + 1);
+		
 		arr[curRow][curCol] = temp;
+		
 		int min = -1;
 		if (right != -1) min = right;
 		if (up != -1 && (min == -1 || up < min)) min = up;
-		if (left != -1 && (min == -1 || left < min)) min = left;
-		if (down != -1 && (min == -1 || down < min)) min = down;
+		
 		return min;
 	}
+	
 	public static int noOfPaths(String[][] arr) {
 		int curRow = arr.length - 1;
 		int curCol = 0;
@@ -52,13 +55,13 @@ public class PracticeProblem {
 		}
 		String temp = arr[curRow][curCol];
 		arr[curRow][curCol] = "V";
+		
+		// Only explore right and up
 		int right = pathsHelper(arr, curRow, curCol + 1);
 		int up = pathsHelper(arr, curRow - 1, curCol);
-		int left = pathsHelper(arr, curRow, curCol -1);
-		int down = pathsHelper(arr, curRow + 1, curCol);
+		
 		arr[curRow][curCol] = temp;
-		return right + up + left + down;
+		
+		return right + up;
 	}
-
-	
 }
